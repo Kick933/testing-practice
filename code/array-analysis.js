37dc2b3b033f82a49e6ad28697141bf503da4664
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-08-03 16:17:05 +0530

    Array Analysis returns object instead of string.

--- code/array-analysis.js
+++ code/array-analysis.js
@@ -1,4 +1,5 @@
 function analyse(arg){
+    let obj = {};
     let sum = 0;
     for(let i =0; i < arg.length; i++){
         sum += arg[i];
@@ -6,12 +7,12 @@ function analyse(arg){
     const sorted = arg.sort((a,b) => a>b ? 1 : -1);
     let max = sorted[arg.length -1]
     let min = sorted[0]
-    
     let avg = sum/arg.length;
-
-
-
-    return `Average: ${avg}, Max: ${max}, Min: ${min}, Length: ${arg.length}`
+    obj['Average'] = avg;
+    obj['Max'] = max;
+    obj['Min'] = min;
+    obj['Length'] = arg.length;
+    return obj;
 }
 
 module.exports = analyse
